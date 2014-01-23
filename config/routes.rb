9bd169715cad5b21efe0f40248eaa473dea24e54
vforteli <verner@fortelius.net>
2014-01-23 13:12:20 +0200

    visitationform scaffold created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Merikotkat::Application.routes.draw do
+  resources :visitation_forms
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
