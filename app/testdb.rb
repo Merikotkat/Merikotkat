a0554d17697f0cd75fbc8996c5292ec29e7c9e13
vforteli <verner@fortelius.net>
2014-01-28 13:54:57 +0200

    db stuff

--- app/testdb.rb
+++ app/testdb.rb
@@ -1,7 +1,7 @@
 # Run this to test db...
 require 'oci8'
 
-oci = OCI8.new('lintukuvat_dev','kuvadev3XT','LTKM')
-oci.exec('select * from table') do |record|
+oci = OCI8.new('lintukuvat_dev','durr','LTKM')
+oci.exec('select * from ljesfsefes') do |record|
   puts record.join(',')
 end
