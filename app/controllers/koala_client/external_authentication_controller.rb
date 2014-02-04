49d5257a683c3ab2639aa21ee92e9be177a7ef0d
vforteli <verner@fortelius.net>
2014-02-04 20:32:31 +0200

    cleanup

--- app/controllers/koala_client/external_authentication_controller.rb
+++ app/controllers/koala_client/external_authentication_controller.rb
@@ -15,7 +15,7 @@ class KoalaClient::ExternalAuthenticationController < ApplicationController
       uri = URI("https://h92.it.helsinki.fi/tipu-api/lintuvaara-authentication-decryptor?key=#{key}&iv=#{iv}&data=#{data}")
       req = ::Net::HTTP::Get.new(uri)
       req.basic_auth Lintuvaara::ApiConfig::SERVER_ACCOUNT, Lintuvaara::ApiConfig::SERVER_PASSWORD
-      res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+      res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|  # Ignore ssl cert errors for dev...
         https.request(req)
       end
 
--- lib/koala_client.rb
+++ lib/koala_client.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/koala_client/configuration'
-require_relative '../lib/koala_client/routes'
-require_relative '../lib/koala_client/authentication'
+require_relative 'koala_client/configuration'
+require_relative 'koala_client/routes'
+require_relative 'koala_client/authentication'
 #require 'koala_client/user'
