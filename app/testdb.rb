2038165570c96e7aa70b9cd96ec53f895c63c98b
vforteli <verner@fortelius.net>
2014-02-04 13:55:36 +0200

    Merge branch 'master' into auth
    Conflicts:
    	Gemfile

--- app/testdb.rb
+++ app/testdb.rb
@@ -1,7 +1,7 @@
 # Run this to test db...
 require 'oci8'
 
-oci = OCI8.new('lintukuvat_dev','durr','LTKM')
-oci.exec('select * from ljesfsefes') do |record|
+oci = OCI8.new('lintukuvat_dev','hurr','ltkm')
+oci.exec('select * from visitation_forms') do |record|
   puts record.join(',')
 end
