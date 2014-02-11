3655cb513b58a274165f225136411fcd832aa095
Vähämaa Ilkka K <ivahamaa@da1-cs-bk107-02.helsinki.fi>
2014-02-11 14:09:51 +0200

    Added style

--- app/testdb.rb
+++ app/testdb.rb
@@ -2,6 +2,8 @@
 require 'oci8'
 
 oci = OCI8.new('lintukuvat_dev','hurr','ltkm')
-oci.exec('select * from visitation_forms') do |record|
+oci.exec('select value
+  from nls_database_parameters
+   where parameter = \'NLS_CHARACTERSET\'') do |record|
   puts record.join(',')
 end
