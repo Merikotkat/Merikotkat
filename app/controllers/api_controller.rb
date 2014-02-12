1c3ba66ed1ea0dc3baf79210d9637c1a9f3f801b
vforteli <verner@fortelius.net>
2014-02-12 20:08:54 +0200

    some refactoring

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,30 +2,19 @@ require 'net/http'
 
 class ApiController < ApplicationController
   def getringer
-    uri = URI("https://h92.it.helsinki.fi/tipu-api/ringers?format=json&filter=#{params['filter']}")
-    req = ::Net::HTTP::Get.new(uri)
-    req.basic_auth Lintuvaara::ApiConfig::SERVER_ACCOUNT, Lintuvaara::ApiConfig::SERVER_PASSWORD
-    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|  # Ignore ssl cert errors for dev...
-      https.request(req)
-    end
+    data = TipuApiHelper.GetApiData(URI("https://h92.it.helsinki.fi/tipu-api/ringers?format=json&filter=#{params['filter']}"))
 
     respond_to do |format|
-        format.json { render json: res.body}
+        format.json { render json: data}
     end
   end
 
 
   def getmunicipalities
-    # not very dry no...
-    uri = URI("https://h92.it.helsinki.fi/tipu-api/municipalities?format=json")
-    req = ::Net::HTTP::Get.new(uri)
-    req.basic_auth Lintuvaara::ApiConfig::SERVER_ACCOUNT, Lintuvaara::ApiConfig::SERVER_PASSWORD
-    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|  # Ignore ssl cert errors for dev...
-      https.request(req)
-    end
+    data = TipuApiHelper.GetApiData(URI("https://h92.it.helsinki.fi/tipu-api/municipalities?format=json"))
 
     respond_to do |format|
-      format.json { render json: res.body}
+      format.json { render json: data}
     end
   end
 end
--- app/models/tipu_api_helper.rb
+++ app/models/tipu_api_helper.rb
@@ -3,21 +3,22 @@ require 'active_support/cache/memory_store'
 
 class TipuApiHelper
     def self.GetMunicipalities
-
       if Rails.cache.read('municipalitiesdropdown').nil?
           puts 'Refreshing municipalities cache'
-          uri = URI("https://h92.it.helsinki.fi/tipu-api/municipalities?format=json")
-          req = ::Net::HTTP::Get.new(uri)
-          req.basic_auth Lintuvaara::ApiConfig::SERVER_ACCOUNT, Lintuvaara::ApiConfig::SERVER_PASSWORD
-          res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|  # Ignore ssl cert errors for dev...
-            https.request(req)
-          end
-
-          @municipalities = JSON.parse res.body
+          @municipalities = JSON.parse GetApiData(URI("https://h92.it.helsinki.fi/tipu-api/municipalities?format=json"))
           @municipalities = @municipalities["municipalities"]["municipality"]
           Rails.cache.write('municipalitiesdropdown', @municipalities, :expires_in => 30.minutes)
       end
-
       return Rails.cache.read('municipalitiesdropdown')
+    end
+
+
+    def self.GetApiData(uri)
+      req = ::Net::HTTP::Get.new(uri)
+      req.basic_auth Lintuvaara::ApiConfig::SERVER_ACCOUNT, Lintuvaara::ApiConfig::SERVER_PASSWORD
+      res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|  # Ignore ssl cert errors for dev...
+        https.request(req)
+      end
+      return res.body
     end
 end
