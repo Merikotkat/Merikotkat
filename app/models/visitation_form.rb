57eefcab1f13ac8a08ccdc4b461e4f21882a192d
Merioksa Mikko E <merioksa@da1-cs-bk107-37.helsinki.fi>
2014-02-13 13:47:09 +0200

    finishing localizationand nest id validation

--- app/models/visitation_form.rb
+++ app/models/visitation_form.rb
@@ -1,3 +1,6 @@
 class VisitationForm < ActiveRecord::Base
   has_many :images
+
+  validates :nest_id, numericality: { only_integer: true, message: I18n.t('error_value_must_be_number') }
+  validates :nest_id, length: { maximum: 5, message: I18n.t('error_value_too_long') }
 end
