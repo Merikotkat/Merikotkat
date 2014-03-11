de0a41ecb6613f36924a3c91cc8f8dfa0b94ec89
coderodde <rodion.efremov@helsinki.fi>
2014-03-11 12:57:48 +0200

    Integraatiotestauksen aloittelua

---
+++ spec/features/main_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "On login page" do
+#  before :each do
+#    FactoryGirl.create :user
+#  end
+
+#  it "should have login link if not yet logged in" do
+#    @user = { login_id: '1', user_name: 'Pekka Murkka', type: 'admin'}
+#    visit root_path
+#    save_and_open_page
+#  end
+end
