ade250ddf08f891670fe6a2af9b4c177a578637d
Merioksa Mikko E <merioksa@da1-cs-bk107-37.helsinki.fi>
2014-03-18 14:43:10 +0200

    bug fixes

--- app/models/visitation_form.rb
+++ app/models/visitation_form.rb
@@ -1,8 +1,9 @@
 class VisitationForm < ActiveRecord::Base
   has_many :images
 
-  validates :nest_id, numericality: { only_integer: true, message: I18n.t('error_value_must_be_number') }
+  validates :nest_id, numericality: { only_integer: true, message: I18n.t('error_value_must_be_number'), allow_nil: true }
   validates :nest_id, length: { maximum: 5, message: I18n.t('error_value_too_long') }
+  validates :nest, presence: { precence: true, message: I18n.t('error_must_be_present') }
 
   if Rails.env.test?
     validates :municipality, inclusion: { in: TipuApiHelperMock.GetMunicipalities.map {|v| v['id'] }, message: I18n.t('error_invalid_municipality')}
