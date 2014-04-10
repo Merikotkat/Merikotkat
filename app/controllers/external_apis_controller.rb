9a0b6bc7c41ce358151cd9389621eed48d87297b
vforteli <verner@fortelius.net>
2014-04-10 11:45:57 +0300

    first version of API, returns image name and id

--- app/controllers/external_apis_controller.rb
+++ app/controllers/external_apis_controller.rb
@@ -13,4 +13,16 @@ class ExternalApisController < BasicauthController
     hurr = images.to_json(:only => [ :id, :filename, :category_id ])
     render json: hurr
   end
+
+
+  def getvisitationforms
+    #todo define fields and format
+    nestid = params[:nestid]
+    speciesid = params[:speciesid]
+    datefrom = params[:datefrom]
+    dateto = params[:dateto]
+
+    forms = VisitationForm.where(nest_id: nestid).where(species_id: speciesid).where(visit_date: datefrom..dateto)
+    render json: forms.to_json
+  end
 end
