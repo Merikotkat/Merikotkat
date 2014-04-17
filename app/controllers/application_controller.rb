8eb58291531930dfb6b5447192499fbbbc7db1d8
Vähämaa Ilkka K <ivahamaa@da1-cs-bk107-33.helsinki.fi>
2014-04-17 15:25:33 +0300

    Error page added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,25 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  if Rails.env.development?
+   # unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: :render_404
+      rescue_from ActionController::RoutingError, with: :render_404
+      rescue_from ActionController::UnknownController, with: :render_404
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    #end
+  end
+
+  def render_404(exception)
+    @not_found_path = exception.message
+    respond_to do |format|
+      format.html { render template: '404', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
+
+
   def mock_usr
     session = { :user => '1', :user_type => 'admin', :user_name => 'Pekka Murkka'}
     @user = {:login_id => session[:user], :type => session[:user_type], :user_name => session[:user_name]}
