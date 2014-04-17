687519547c06611d134008620808289639e9e675
Merioksa Mikko E <merioksa@da1-cs-bk107-34.helsinki.fi>
2014-04-17 11:25:55 +0300

    some more cleaning

--- app/models/image.rb
+++ app/models/image.rb
@@ -15,7 +15,7 @@ class Image < ActiveRecord::Base
     extension = File.extname(image.filename).downcase
     accepted_formats = [".png", ".jpg", ".jpeg"]
     if !accepted_formats.include? extension
-      image.errors[:content_type] << I18n.t('error_file_type_unsupported') + extension
+      image.errors[:content_type] << I18n.t('error_file_type_unsupported', type: extension)
     end
   end
 
@@ -33,28 +33,8 @@ class Image < ActiveRecord::Base
     end
   end
 
-  # Create temporary image files for all the form's images, and return the paths in an array
-  def self.get_all_images(form_id)
-    form = VisitationForm.find(form_id)
-
-    arr = Array.new
-
-    img_index = 0
-    form.images.each do |img|
-      File.open('public/images/temp' + img_index.to_s, 'wb') { |file| file.write(img.thumbnaildata.nil? ? img.data : img.thumbnaildata) }
-      arr.push ( {:id => img.id, :original_filename => img.filename, :temp_path => 'temp' + img_index.to_s} )
-      img_index += 1
-    end
-
-    return arr
-  end
-
-  def self.get_bird1_images(form_id)
+  def self.get_adult_images(form_id)
     return get_images_of_type form_id, 1
-  end
-
-  def self.get_bird2_images(form_id)
-    return get_images_of_type form_id, 2
   end
 
   def self.get_young_images(form_id)
@@ -74,15 +54,9 @@ class Image < ActiveRecord::Base
 
     arr = Array.new
 
-    #img_index = 0
-
     images.each do |img|
       if img.image_type == type
-        #temp_folder = 'public/images/'
-        #temp_filename = 'temp' + type.to_s + img_index.to_s
-        #File.open(temp_folder + temp_filename, 'wb') { |file| file.write(img.thumbnaildata.nil? ? img.data : img.thumbnaildata) }
         arr.push ( img )
-        #img_index += 1
       end
     end
 
