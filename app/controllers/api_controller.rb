945201ac47ca13852030254741e3b91e78ed0ee5
vforteli <verner@fortelius.net>
2014-04-22 13:13:44 +0300

    localisation support for municipalities

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,34 +7,30 @@ class ApiController < ApplicationController
   def getringer
     filter = url_encode(params['filter'])
     data = TipuApiHelper.GetApiData(URI("https://h92.it.helsinki.fi/tipu-api/ringers?format=json&filter=#{filter}"))
-
-    respond_to do |format|
-      format.json { render json: data}
-    end
+    render json: data
   end
 
 
   def getmunicipalities
-    # Updated to get the municipalities from the cache instead and do the filtering afterwards
     filter = UnicodeUtils.upcase(params['filter'])
     municipalities = TipuApiHelper.GetMunicipalities
-
-    #todo determine which fields should be searched, should it depend on locale?
-    data = municipalities.select{ |herp| herp['id'].start_with?(filter) || herp['name'][0]['content'].start_with?(filter) }
+    data = municipalities.select{ |s| s['id'].start_with?(filter) || s['name'].any?{ |s| !s['content'].nil? && UnicodeUtils.upcase(s['content']).start_with?(filter) } }
     render json: {municipalities: { municipality: data }}
   end
+
 
   def getspecies
     filter = UnicodeUtils.upcase(params['filter'])
     species = TipuApiHelper.GetSpecies
-    data = species['species'].select{ |herp| herp['id'].start_with?(filter) || herp['name'][0]['content'].upcase.start_with?(filter) }
+    data = species['species'].select{ |s| s['id'].start_with?(filter) || s['name'].any?{ |s| UnicodeUtils.upcase(s['content']).start_with?(filter)} }
     render json: { species: data }
   end
 
+
   def getcolors
-    filter = UnicodeUtils.downcase(params['filter'])
+    filter = UnicodeUtils.upcase(params['filter'])
     colors = TipuApiHelper.GetColors
-    data = colors['codes']['code'].select{ |herp| herp['code'].downcase.start_with?(filter) || herp['desc'][0]['content'].downcase.start_with?(filter)}
+    data = colors['codes']['code'].select{ |s| s['code'].upcase.start_with?(filter) || s['desc'].any?{ |s| UnicodeUtils.upcase(s['content']).start_with?(filter)}}
     render json: { codes: data }
   end
 end
